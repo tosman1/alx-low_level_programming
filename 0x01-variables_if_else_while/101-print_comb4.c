eb2ae7ef68101958f65c7226a66bf9f7d736a548
tosman1 <adeoomo20@yahoo.com>
2022-12-10 22:33:34 -0800

    variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,10 +13,11 @@ int main(void)
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
-		{
+
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundreds > tens)))
 				{
 					putchar(hundreds);
 					putchar(tens);
