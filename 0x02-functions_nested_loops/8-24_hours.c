03d5213b42a7a455763f4bea0345ada80f82be86
tosman1 <adeoomo20@yahoo.com>
2022-12-13 14:23:53 -0800

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,21 +2,27 @@
 
 /**
  * jack_bauer - print every minute of the day
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-	int hour, minute;
+	int i,j;
 
-	for (hour = o, hour <= 23; hour++)
+	i = 0;
+
+	while (i < 24)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		j = 0;
+		while (j < 60)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(',');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
