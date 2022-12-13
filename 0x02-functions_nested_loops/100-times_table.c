deb6fe68a8d7888be91a93ea2b43874cd8563099
tosman1 <adeoomo20@yahoo.com>
2022-12-13 13:28:30 -0800

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+				 else if (k < 10 && j != 0);
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					 _putchar((k / 10) + '0');
 					 _putchar((k % 10) + '0');
 				 } else if (k >= 100)
-				{
+				 {
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -22,6 +22,7 @@ int main(void)
 	    _putchar(',');
 	    _putchar(' ');
 	    _putchar(r + '0');
+	    _putchar('\n');
 	    r = print_sign(0);
 	    _putchar(',');
 	    _putchar(' ');
