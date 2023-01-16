277904c0754d5aa5a56b9d2e260e6707aad17fbe
tosman1 <adeoomo20@yahoo.com>
2023-01-16 05:37:25 -0800

    update on task structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,23 +5,30 @@
 #include <stdlib.h>
 
 /**
- *  * struct dog - dog
- *   * @name: name of the dog
- *    * @age: how old the dog is
- *     * @owner: who owns the dog
- *      */
-typedef struct dog
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
 {
-		char *name;
-			float age;
-				char *owner;
-} dog_t;
+	char *name;
+	float age;
+	char *owner;
+};
 
-int _strlen(char *s);
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
